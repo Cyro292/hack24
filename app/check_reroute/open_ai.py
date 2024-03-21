8d0fa9314d9b4b845ce5bfdf4e52d4d821cad7f1
Cyro292 <amon@archi.de>
2024-03-21 14:01:15 +0100

    fixed some bugs

--- app/check_reroute/open_ai.py
+++ app/check_reroute/open_ai.py
@@ -5,20 +5,20 @@ from dotenv import load_dotenv
 load_dotenv()
 
 
-async def get_reroute_response_from_question(question: str):
+async def get_reroute_response_from_question(question: str, context: str):
 
     client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
 
     response = client.chat.completions.create(
-        model=os.getenv("REROUTE_OPENAI_MODEL"),
+        model="gpt-4-turbo",
         messages=[
             {
                 "role": "system",
-                "content": "You need to decide if the reroute and need to decide with a number from 0 to 1 if the user wants a redirect to a human expert rather then the AI. 0 means no reroute and 1 means reroute. The var should be named reroute.",
+                "content": f"{context} You need to decide if the reroute and need to decide with a number from 0 to 1 if the user wants a redirect to a human expert rather then the AI. 0 means no reroute and 1 means reroute. The var should be named reroute.",
             },
             {"role": "user", "content": question},
         ],
-        temperature=0.7,
+        temperature=0.4,
         response_format={"type": "json_object"},
     )
 
