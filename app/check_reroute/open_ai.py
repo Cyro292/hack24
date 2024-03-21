cbd6135a5439a25989c4b56e7b71ba44d3b1d38d
louiskhub <lkapp@uni-osnabrueck.de>
2024-03-21 14:25:08 +0100

    improve prompt

--- app/check_reroute/open_ai.py
+++ app/check_reroute/open_ai.py
@@ -5,18 +5,18 @@ from dotenv import load_dotenv
 load_dotenv()
 
 
-async def get_reroute_response_from_question(question: str, context: str):
-
+async def get_reroute_response_from_question(question: str, contexts: list[str]):
     client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
 
+    context = "\n".join(contexts)
     response = client.chat.completions.create(
         model="gpt-4-turbo",
         messages=[
             {
                 "role": "system",
-                "content": f"{context} \n \n Gebe den value reroute zurück. Dieser Value liegt zwischen 0 und 1 und gibt an, ob ein reroute notwendig ist basierend darauf ob die Frage gut genug beantwortet wurde oder der nutzer nach einem menschlichen experten fragt.",
+                "content": f"Gebe den Wert \"reroute\" zurück. Dieser Value liegt zwischen 0 und 1 und gibt an, wie relevant der Kontext zur gestellten Frage ist. Wenn der Kontext irrelevant ist, soll \"reroute\" nahe an 0 sein. Wenn der Kontext sehr relevant ist soll \"reroute\" nahe an 1 sein. Kontext:\n\"\"\"{context}\"\"\"\n\n",
             },
-            {"role": "user", "content": question},
+            {"role": "user", "content": f'Frage:\n\"\"\"{question}\"\"\"'},
         ],
         temperature=0.4,
         response_format={"type": "json_object"},
