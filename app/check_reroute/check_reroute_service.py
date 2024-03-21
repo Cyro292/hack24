79c8059c3eebf4f53c4f002ba6b2f402592b3af3
Cyro292 <amon@archi.de>
2024-03-21 14:05:56 +0100

    update reroute again

--- app/check_reroute/check_reroute_service.py
+++ app/check_reroute/check_reroute_service.py
@@ -1,16 +1,16 @@
 from .open_ai import get_reroute_response_from_question
 
 
-async def get_reroute_nessary(question: str):
-    response = await get_reroute_response_from_question(question)
-    
+async def get_reroute_nessary(question: str, context: str):
+    response = await get_reroute_response_from_question(question, context)
+
     content = response.choices[0].message["content"]
-    
-    if not content['reroute']:
+
+    if not content["reroute"]:
         print("Something went wrong with the reroute")
         return False
-    
-    if content['reroute'] > 0.5:
+
+    if content["reroute"] > 0.5:
         return True
-    
+
     return False
